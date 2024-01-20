add608873933edf14f808c4cd3c0aa0feec0f8c7
EvilWolf <vladimir-vavin@ya.ru>
2024-01-20 16:36:11 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,6 @@
+public class Line implements Figure{
+    @Override
+    public void draw(String color) {
+
+    }
+}
---
+++ src/CompositeFigure.java
@@ -0,0 +1,6 @@
+public class Triangle implements Figure{
+    @Override
+    public void draw(String color) {
+        System.out.println("triangle");
+    }
+}
